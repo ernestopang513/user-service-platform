f2cbf02168b6c843ae9c3a45676cb34a655c1cff
Ernesto Pang <ernestopang513@hotmail.com>
2025-10-29 00:55:18 -0600

    feat: add cors config

--- src/main/java/com/ernesto/backend/user_service_platform/security/SpringSecurityConfig.java
+++ src/main/java/com/ernesto/backend/user_service_platform/security/SpringSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.ernesto.backend.user_service_platform.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -12,6 +16,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.ernesto.backend.user_service_platform.repositories.UserRepository;
 import com.ernesto.backend.user_service_platform.security.filter.JwtAuthenticationFilter;
@@ -49,7 +57,34 @@ public class SpringSecurityConfig {
         .addFilter(new JwtAuthenticationFilter(authenticationManager(), userRepository))
         .addFilter(new JwtValidationFilter(authenticationManager(), userRepository))
         .csrf(config -> config.disable())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()) )
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        config.setAllowCredentials(true);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> corsBean = new FilterRegistrationBean<>(
+            new CorsFilter(corsConfigurationSource()));
+            corsBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+            return corsBean;
+        
     }
 }
